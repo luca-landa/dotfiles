9a73af1c595fe9ff9321c223950ffbfdb8e4b9f2
luca-landa <lucalanda@hotmail.it>
2022-04-12 16:17:40 +0200

    Improve and update full vim configuration

--- setup.sh
+++ setup.sh
@@ -9,9 +9,12 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 brew install python
 brew install neovim
 pip3 install --user neovim
+
 mkdir -p ~/.vim/bundle
+mkdir -p ~/.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' # install vim-plug
+mv ./config/nvim/init.vim ~/.config/nvim/init.vim
 vim +PluginInstall
 
 git config --global core.excludesfile ~/.gitignore_global
