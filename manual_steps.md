50b4b80c4fb822434465e01895b16fdb8fbad1ae
Luca Landa <lucalanda@Lucas-MacBook-Pro-2.local>
2022-06-24 19:31:21 +0200

    Fix setup configuration
    Apply fixes that were required while setting up the new MacBook

---
+++ manual_steps.md
@@ -0,0 +1,18 @@
+# Manual steps
+* copy .ssh folder with private keys
+* Load iTerm2 configuration from ~/iterm2_hotkey_window_profile.json
+* Install nvim language servers (LspInstall <language_server>)
+  * bashls
+  * cssls
+  * ccls
+  * eslint
+  * html
+  * jsonls
+  * marksman
+  * pyright
+  * solargraph
+  * tailwindcss
+  * vuels
+  * lemminx
+  * yamlls
+
---
+++ setup_mac.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+RUBY_VERSION=latest
+NODE_VERSION=latest
+POSTGRES_VERSION=14.4
+
+set -e
+
+cp -r ./home/. ~/
+sudo cp ./etc/hosts /etc/hosts
+mkdir ~/.postgres
+
+xcode-select --install
+
+bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install zsh
+chsh -s /usr/local/bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+brew install python
+brew install neovim
+pip3 install --user neovim
+
+brew tap homebrew/cask-versions
+
+brew install gcc llvm http-server readline zlib ossp-uuid coreutils libpq nss imagemagick curl mkcert git gpg gawk zsh yarn asdf
+brew install stripe/stripe-cli/stripe
+
+asdf plugin add ruby
+asdf install ruby $RUBY_VERSION
+asdf global ruby $RUBY_VERSION
+
+asdf plugin add nodejs
+asdf install nodejs $NODE_VERSION
+asdf global nodejs $NODE_VERSION
+
+asdf plugin add postgres
+asdf install postgres $POSTGRES_VERSION
+
+brew install fzf
+brew install ripgrep
+brew install libyaml
+brew install git
+brew install gh
+brew install thefuck
+brew install mas
+brew install redis
+brew install shared-mime-info
+
+brew install --cask iterm2
+brew install --cask vscodium
+brew install --cask slack
+brew install --cask zoom
+brew install --cask whatsapp
+brew install --cask google-chrome
+brew install --cask google-chat
+brew install --cask postman
+brew install --cask spectacle
+brew install --cask firefox-developer-edition
+
+brew tap heroku/brew && brew install heroku
+
+mas install 506189836 # Harvest
+
+apps_to_open_at_login=(Harvest iTerm Slack Spectacle)
+for app in $apps_to_open_at_login
+  osascript -e  "tell application \"System Events\" to make login item at end with properties { path: \"/Applications/$app.app\", hidden: false }"
+
+mkdir -p ~/.config/nvim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo cp ./config/nvim/init.vim ~/.config/nvim/init.vim
+sudo chown -R $USER ~/.vim ~/.local ~/.config /Library/Ruby
+vim +PluginInstall
+
+git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name "luca-landa"
+git config --global user.email "lucalanda@hotmail.it"
